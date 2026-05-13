11dca8156714455cfa999ad50de2962f78c49767
Erik Bosman <erik@minemu.org>
2026-05-13 09:00:26 +0200

    change USB descriptor for popcalc

--- firmware/popcalc/config.h
+++ firmware/popcalc/config.h
@@ -3,9 +3,9 @@
 
 #include <libopencm3/stm32/gpio.h>
 
-#define MANUFACTURER_STRING "example co."
-#define PRODUCT_STRING "usb hid demo"
-#define SERIAL_STRING "00000001"
+#define MANUFACTURER_STRING "YourBoySerge inc."
+#define PRODUCT_STRING "popcalc!"
+#define SERIAL_STRING "42424242"
 
 #define F_CPU (48000000)
 #define F_SYS_TICK_CLK (F_CPU/8)
